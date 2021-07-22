ce5c982e2cf53fd39aafb54b696e00bfd81e5abf
Dylan Mld <lucenzo.76@gmail.com>
2021-07-22 14:13:40 +0200

    fix(utils): missing patterntoroute function.

--- src/server/util.lua
+++ src/server/util.lua
@@ -141,3 +141,23 @@ function PromiseAsync:Series(tasks)
     end)
     return p
 end
+
+---@private
+--- Converts a string pattern "`/users/:id`" to an object for the Request class to use
+function PatternToRoute(input)
+    if input == "/" then input = "" end
+    local routeData = {
+        route = input,
+        params = {}
+    }
+    local matcher = input
+    for k,v in input:gmatch "/(:%w+)" do
+        local rawname = k:gsub(":", "")
+        table.insert(routeData.params, {
+            name = rawname
+        })
+        matcher, _ = matcher:gsub(k, "(%%w+)", 1)
+    end
+    routeData.route = matcher
+    return routeData
+end
